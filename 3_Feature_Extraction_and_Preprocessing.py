485b32ea11a732f75540b0965c6e8567152f66e5
Adam Liu <adamliu.s7@gmail.com>
2016-11-09 12:41:10 +1100

    some updates

--- 3_Feature_Extraction_and_Preprocessing.py
+++ 3_Feature_Extraction_and_Preprocessing.py
@@ -1,9 +1,6 @@
-from sklearn import datasets
-digits = datasets.load_digits()
-print ("Digit: ", digits.target[0])
-print (digits.images[0])
-print ('Feature vector:\n', digits.images[0].reshape(-1, 64))
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
-
-
-
+s = pd.Series([1,3,5,np.nan,6,8])
+print(s)
