8a459b5316af12de18d41494c029c5e8a22b8b06
Adam Liu <adamliu.s7@gmail.com>
2016-12-07 09:17:35 +1100

    adding r notebook

--- test.py
+++ test.py
@@ -1,32 +1,12 @@
+
 import numpy as np
 import pandas as pd
 
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.linear_model.logistic import LogisticRegression
-from sklearn.cross_validation import train_test_split, cross_val_score
+dates = pd.date_range("1/2/2000", periods = 8)
 
-df = pd.read_csv('data/SMSSpamCollection/SMSSpamCollection', delimiter='\t', header=None)
+df = pd.DataFrame(np.random.rand(8, 4), index = dates, columns = ["A", "B", "C", "D"])
 
-X_train_raw, X_test_raw, y_train, y_test = train_test_split(df[1], df[0])
-
-vectorizer = TfidfVectorizer()
-X_train = vectorizer.fit_transform(X_train_raw)
-X_test = vectorizer.transform(X_test_raw)
-
-classifier = LogisticRegression()
-classifier.fit(X_train, y_train)
-predictions = classifier.predict(X_test)
-for i, prediction in enumerate(predictions[:5]):
-  print (X_test_raw[i], 'prediction:', prediction)
-
-
-
-
-
-
-
-
-
+print (df)
 
 
 
